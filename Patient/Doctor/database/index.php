a59489f9a836d6e39a5bac31a00ffe7adfe855e8
Lynn Riddlespur <webdevdea@gmail.com>
2013-09-21 13:01:05 -0400

    Create index.php
    Index.php calls upon the controller to get the whole thing started

---
+++ Patient/Doctor/database/index.php
@@ -0,0 +1,9 @@
+<?php
+
+// prevent local page caching
+header("Cache-Control: no-cache, must-revalidate"); 
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); 
+
+require_once('Controller.php');
+
+?>
